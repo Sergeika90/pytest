dd7f0a496ef54b678a8f7f22eb4a544ad06f821e
Sergeika90 <skat_newtown@mail.ru>
2020-06-29 18:01:31 +0400

    изменил название на items

---
+++ test_items.py
@@ -0,0 +1,7 @@
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/" 
+ 
+def test_items(browser):
+    browser.get(link)
+    button = browser.find_element_by_class_name("btn-add-to-basket")
+    
+    assert button == button, "there is no button"
